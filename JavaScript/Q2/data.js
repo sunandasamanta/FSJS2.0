54b7c5a4c515fa782960929ce3fdd3d2f03f1e8a
sunandasamanta <samantasunanda724@gmail.com>
2023-05-20 16:06:14 +0530

    question 2, interpolation method

---
+++ JavaScript/Q2/data.js
@@ -0,0 +1,11 @@
+var firstName = "Sunanda";
+var lastName = "Samanta";
+var age = 23;
+var married = false;
+var country = "India";
+
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Age: " + age);
+console.log("Married: " + married);
+console.log("Country: " + country);
