1ab758d50b5e25c7684005fede5896c0ff3e38c4
sunandasamanta <samantasunanda724@gmail.com>
2023-05-20 15:53:44 +0530

    question 1 and solution

---
+++ JavaScript/Q1/variable.js
@@ -0,0 +1,13 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type
+
+var nam = "Sunanda-Samanta";
+var age = 23;
+var employed = false;
+var salary = null;
+var address;
+
+console.log("The value of nam is " + nam + " and the type is " + typeof nam);
+console.log("The value of age is " + age + " and the type is " + typeof age);
+console.log("The value of employed is " + employed + " and the type is " + typeof employed);
+console.log("The value of salary is " + salary + " and the type is " + typeof salary);
+console.log("The value of address is " + address + " and the type is " + typeof address);
