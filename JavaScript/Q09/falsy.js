b19813d92461d635df0318639ee4f189cf52e097
sunandasamanta <samantasunanda724@gmail.com>
2023-05-24 07:39:43 +0530

    truthy and falsy values

---
+++ JavaScript/Q09/falsy.js
@@ -0,0 +1,5 @@
+const num = "13";
+console.log(num === 13);
+const naam = "Bakshi";
+console.log(naam === "Sunanda" && "Sunanda");
+console.log(0 && "Bakshi");
---
+++ JavaScript/Q09/truthy.js
@@ -0,0 +1,6 @@
+const naam = "Sunanda";
+console.log(naam === "Sunanda" && "Sunanda");
+const num = "13";
+console.log(num == 13);
+const age = 23;
+console.log(age === 23);
